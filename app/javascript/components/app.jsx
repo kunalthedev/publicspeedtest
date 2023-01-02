aa2fe26c1dcb091873fef47e3f34786a7724b3bc
kunal <10kunalji@gmail.com>
2023-01-02 20:27:51 +0530

    setted up react inside the project

---
+++ app/javascript/components/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <div>Hi </div>;
+}
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
